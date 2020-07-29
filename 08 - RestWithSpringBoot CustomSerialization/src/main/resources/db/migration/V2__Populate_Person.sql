1278a50316725b8917f3eeab0a8c56c33c6eb0da
NilsonAndre <nilson.andre@gmail.com>
2020-07-28 22:46:39 -0300

    CustomSerialization

---
+++ 08 - RestWithSpringBoot CustomSerialization/src/main/resources/db/migration/V2__Populate_Person.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.person VALUES (1, 'Araucária-Paraná-Brasil', 'Nilson', 'Male', 'André Machniewicz');
+INSERT INTO public.person VALUES (2, 'Araucária-Paraná-Brasil', 'Tatiane', 'Female', 'Dudek');
